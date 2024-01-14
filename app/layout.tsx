a7c3eaced9120c01c1f462bbf271a4c1072601e8
JocoBadique <badiquejoco@gmail.com>
2024-01-14 15:00:30 +0800

    OG

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   openGraph: {
-    title: "Joco Badiques",
+    title: "Joco Badiquer",
     description: "My personal portfolio app with space vibe sheeshh!✨",
     url: "https://nextjs.org",
     siteName: "Joco Badique",
@@ -15,11 +15,13 @@ export const metadata: Metadata = {
         url: "/preview1.png",
         width: 800,
         height: 600,
+        type: "image/png",
       },
       {
         url: "/preview1.png",
         width: 1800,
         height: 1600,
+        type: "image/png",
         alt: "Joco Badique",
       },
     ],
